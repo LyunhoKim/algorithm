d671fc43553cc519d48764fc92248dea7e654ef1
Lyunho Kim <kimlh2@gmail.com>
2016-12-13 18:29:52 +0900

    1단계

---
+++ NQueen.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	static int [][] table;
+	static int tableSize;
+	static int cntPossibleQueen;
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		while(T-- > 0) {
+			tableSize = sc.nextInt();
+			cntPossibleQueen = 0;
+			table = new int [tableSize][tableSize];
+			init();
+			
+			findNQueen(0);
+			
+			System.out.println(cntPossibleQueen);
+			
+			
+		}
+		
+	}
+	
+	private static void init() {
+		for(int i=0; i<tableSize; i++)
+			for(int j=0; j<tableSize; j++)
+				table[i][j] = 0;
+	}
+
+	private static void findNQueen(int depth) {
+		if(depth == tableSize) {
+			cntPossibleQueen++;
+			return;
+		}
+			
+		
+		for(int i=0; i<tableSize; i++) {
+			clearBoardRow(depth);
+			if(isPossiblePosition(depth, i)) {
+				table[depth][i] = 1; 
+				findNQueen(depth+1);
+			}
+		}
+		
+		
+	}
+	
+	private static void clearBoardRow(int depth) {
+		for(int i=0; i<tableSize; i++)
+			table[depth][i] = 0;
+	}
+
+	private static boolean isPossiblePosition(int depth, int col) {
+		for(int i=0; i<depth; i++) 
+			if(table[i][col] != 0)
+				return false;
+		
+		for(int i=depth, j=col; i>=0 && j>=0; i--, j--)
+			if(table[i][j] != 0)
+				return false;
+		
+		for(int i=depth, j=col; i>=0 && j<tableSize; i--, j++)
+			if(table[i][j] != 0)
+				return false;
+		
+		return true;
+	}
+
+}
